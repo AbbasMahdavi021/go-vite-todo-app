acb538279e595dc5a5143d5bc04a6e4992bada80
Abbas Mahdavi <abbas.mahdavi021@gmail.com>
2024-05-17 03:13:16 -0700

    configure concurrently, tailwind, and sqlite3 database (#2)

---
+++ db.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+var DB *sql.DB
+
+func openDB() error {
+	db, err := sql.Open("sqlite3", "./sqlite3.db")
+	if err != nil {
+		return err
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	
+	DB = db
+	return nil
+}
+
+func closeDB() error {
+	return DB.Close()
+}
+
+
+func setupDB() error {
+	_, err := DB.Exec(`create table if not exists tasks (id integer not null primary key, title text, completed boolean default false, position integer);`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
